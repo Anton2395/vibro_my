87025a7d37261435a41e573eb3e654bedd0cb423
Davidovskii-Nikita <davidovskiinikita@gmail.com>
2022-03-24 12:13:22 +0300

    set rewrite 1 minuts

--- work_dir/back_work_snap.py
+++ work_dir/back_work_snap.py
@@ -51,7 +51,7 @@ class StartProcessOpcForConnectToPLC(Process):
             "type": "real",
             "time_sleep": 2000,
             "if_change": True,
-            "rewrite_time": 30
+            "rewrite_time": 1
         },
         {
             "name": "temp",
@@ -59,7 +59,7 @@ class StartProcessOpcForConnectToPLC(Process):
             "type": "real",
             "time_sleep": 2000,
             "if_change": True,
-            "rewrite_time": 30
+            "rewrite_time": 1
         }
         ]
         self.bind = {}
